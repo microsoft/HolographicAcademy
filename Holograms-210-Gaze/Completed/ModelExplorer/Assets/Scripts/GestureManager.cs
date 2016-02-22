d3f976fc26030bc789cdeced4dc36a1ad1397e50
Kurtis Eveleigh <kurtie@microsoft.com>
2016-02-22 12:16:19 -0800

    Adding Holograms 210 - Gaze (Completed)

---
+++ Holograms-210-Gaze/Completed/ModelExplorer/Assets/Scripts/GestureManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.VR.WSA.Input;
+
+/// <summary>
+/// GestureManager contains event handlers for subscribed gestures.
+/// </summary>
+public class GestureManager : MonoBehaviour
+{
+    private GestureRecognizer gestureRecognizer;
+
+    void Start()
+    {
+        gestureRecognizer = new GestureRecognizer();
+        gestureRecognizer.SetRecognizableGestures(GestureSettings.Tap);
+
+        gestureRecognizer.TappedEvent += (source, ray) =>
+        {
+            GameObject focusedObject = InteractibleManager.Instance.FocusedGameObject;
+
+            if (focusedObject != null)
+            {
+                focusedObject.SendMessage("OnSelect");
+            }
+        };
+
+        gestureRecognizer.StartCapturingGestures();
+    }
+
+    void OnDestroy()
+    {
+        gestureRecognizer.StopCapturingGestures();
+    }
+}
---
+++ Holograms-210-Gaze/Completed/ModelExplorer/Assets/Scripts/Interactible.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// The Interactible class flags a Game Object as being "Interactible".
+/// Determines what happens when an Interactinle is being gazed at.
+/// </summary>
+public class Interactible : MonoBehaviour
+{
+    [Tooltip("Audio clip to play when interacting with this hologram.")]
+    public AudioClip TargetFeedbackSound;
+    private AudioSource audioSource;
+
+    private Material[] defaultMaterials;
+    private Material[] materialsWithHightlight;
+    private Material highlightMaterial;
+
+    void Start()
+    {
+        defaultMaterials = GetComponent<Renderer>().materials;
+
+        highlightMaterial = Resources.Load("Materials/AdditiveRimShader", typeof(Material)) as Material;
+
+        // Add highlightMaterial to materialsWithHightlight.
+        materialsWithHightlight = new Material[defaultMaterials.Length + 1];
+        for (int i = 0; i < defaultMaterials.Length; i++)
+        {
+            materialsWithHightlight[i] = defaultMaterials[i];
+        }
+        materialsWithHightlight[materialsWithHightlight.Length - 1] = highlightMaterial;
+
+        // Add a BoxCollider if the interactible does not contain one.
+        Collider collider = GetComponentInChildren<Collider>();
+        if (collider == null)
+        {
+            gameObject.AddComponent<BoxCollider>();
+        }
+
+        EnableAudioHapticFeedback();
+    }
+
+    private void EnableAudioHapticFeedback()
+    {
+        // If this hologram has an audio clip, add an AudioSource with this clip.
+        if (TargetFeedbackSound != null)
+        {
+            audioSource = GetComponent<AudioSource>();
+            if (audioSource == null)
+            {
+                audioSource = gameObject.AddComponent<AudioSource>();
+            }
+
+            audioSource.clip = TargetFeedbackSound;
+            audioSource.playOnAwake = false;
+            audioSource.spatialBlend = 1;
+            audioSource.dopplerLevel = 0;
+        }
+    }
+
+    void LateUpdate()
+    {
+        Debug.ClearDeveloperConsole();
+    }
+
+    /* TODO: DEVELOPER CODING EXERCISE 2.d */
+
+    void GazeEntered()
+    {
+        if (highlightMaterial != null)
+        {
+            // 2.d: Set GetComponent Renderer's materials to
+            // materialsWithHighlight when gazed at.
+            GetComponent<Renderer>().materials = materialsWithHightlight;
+        }
+    }
+
+    void GazeExited()
+    {
+        if (highlightMaterial != null)
+        {
+            // 2.d: Set GetComponent Renderer's materials to 
+            // defaultMaterials when gazed away.
+            GetComponent<Renderer>().materials = defaultMaterials;
+        }
+    }
+
+    void OnSelect()
+    {
+        // Play the audioSource haptic feedback when we gaze and select a hologram.
+        if (audioSource != null && !audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+
+        /* TODO: DEVELOPER CODING EXERCISE 6.a */
+        // 6.a: Handle the OnSelect by sending a PerformTagAlong message.
+        this.SendMessage("PerformTagAlong");
+    }
+}
