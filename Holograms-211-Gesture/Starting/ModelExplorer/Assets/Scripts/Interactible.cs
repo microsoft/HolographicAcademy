598f565f113c06671462fdacc9f8faf33b0c173b
Kurtis Eveleigh <kurtie@microsoft.com>
2016-02-22 15:06:50 -0800

    [Gesture] Updating a longstanding typo in Interactible and removing some extraneous PlayerSettings

--- Holograms-211-Gesture/Starting/ModelExplorer/Assets/Scripts/Interactible.cs
+++ Holograms-211-Gesture/Starting/ModelExplorer/Assets/Scripts/Interactible.cs
@@ -24,7 +24,7 @@ public class Interactible : MonoBehaviour
     private AudioSource audioSource;
 
     private Material[] defaultMaterials;
-    private Material[] materialsWithHightlight;
+    private Material[] materialsWithHighlight;
     private Material highlightMaterial;
 
     void Start()
@@ -33,13 +33,13 @@ public class Interactible : MonoBehaviour
 
         highlightMaterial = Resources.Load("Materials/AdditiveRimShader", typeof(Material)) as Material;
 
-        // Add highlightMaterial to materialsWithHightlight.
-        materialsWithHightlight = new Material[defaultMaterials.Length + 1];
+        // Add highlightMaterial to materialsWithHighlight.
+        materialsWithHighlight = new Material[defaultMaterials.Length + 1];
         for (int i = 0; i < defaultMaterials.Length; i++)
         {
-            materialsWithHightlight[i] = defaultMaterials[i];
+            materialsWithHighlight[i] = defaultMaterials[i];
         }
-        materialsWithHightlight[materialsWithHightlight.Length - 1] = highlightMaterial;
+        materialsWithHighlight[materialsWithHighlight.Length - 1] = highlightMaterial;
 
         // Add a BoxCollider if the interactible does not contain one.
         Collider collider = GetComponentInChildren<Collider>();
@@ -78,7 +78,7 @@ public class Interactible : MonoBehaviour
     {
         if (highlightMaterial != null)
         {
-            GetComponent<Renderer>().materials = materialsWithHightlight;
+            GetComponent<Renderer>().materials = materialsWithHighlight;
         }
     }
 
