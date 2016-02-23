6f149f1350c9f3759e36d68b4bbab3ff704f35ee
Kurtis Eveleigh <kurtie@microsoft.com>
2016-02-22 18:00:31 -0800

    Serializing Origami assets to text

--- Holograms-101/Catch up Chapters/8-Finale/Assets/Scripts/GazeGestureManager.cs
+++ Holograms-101/Catch up Chapters/8-Finale/Assets/Scripts/GazeGestureManager.cs
@@ -17,7 +17,7 @@ public class GazeGestureManager : MonoBehaviour
 
         // Set up a GestureRecognizer to detect Select gestures.
         recognizer = new GestureRecognizer();
-        recognizer.TappedEvent += (source, ray) =>
+        recognizer.TappedEvent += (source, tapCount, ray) =>
         {
             // Send an OnSelect message to the focused object and its ancestors.
             if (FocusedObject != null)
