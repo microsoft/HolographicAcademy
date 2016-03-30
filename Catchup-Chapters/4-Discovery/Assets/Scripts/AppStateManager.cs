71844de1d505f3a15a438442da8b50523fce3474
Neeraj Wadhwa <neerajw@microsoft.com>
2016-03-30 10:01:06 -0700

    SharedHolograms catchup chapter 4 Discovery
    Use this to recover to a working state.

---
+++ Catchup-Chapters/4-Discovery/Assets/Scripts/AppStateManager.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using HoloToolkit.Unity;
+
+/// <summary>
+/// Keeps track of the current state of the experience.
+/// </summary>
+public class AppStateManager : Singleton<AppStateManager>
+{
+    /// <summary>
+    /// Enum to track progress through the experience.
+    /// </summary>
+    public enum AppState
+    {
+        Starting = 0,
+        WaitingForAnchor,
+        WaitingForStageTransform,
+        PickingAvatar,
+        Ready
+    }
+
+    /// <summary>
+    /// Tracks the current state in the experience.
+    /// </summary>
+    public AppState CurrentAppState { get; set; }
+
+    void Start()
+    {
+        // We start in the 'picking avatar' mode.
+        CurrentAppState = AppState.PickingAvatar;
+
+        // We start by showing the avatar picker.
+        PlayerAvatarStore.Instance.SpawnAvatarPicker();
+    }
+
+    void Update()
+    {
+        switch (CurrentAppState)
+        {
+            case AppState.PickingAvatar:
+                // Avatar picking is done when the avatar picker has been dismissed.
+                if (PlayerAvatarStore.Instance.PickerActive == false)
+                {
+                    CurrentAppState = AppState.WaitingForAnchor;
+                }
+                break;
+            case AppState.WaitingForAnchor:
+                if (ImportExportAnchorManager.Instance.AnchorEstablished)
+                {
+                    CurrentAppState = AppState.WaitingForStageTransform;
+                    GestureManager.Instance.OverrideFocusedObject = HologramPlacement.Instance.gameObject;
+                }
+                break;
+            case AppState.WaitingForStageTransform:
+                // Now if we have the stage transform we are ready to go.
+                if (HologramPlacement.Instance.GotTransform)
+                {
+                    CurrentAppState = AppState.Ready;
+                    GestureManager.Instance.OverrideFocusedObject = null;
+                }
+                break;
+        }
+    }
+}
---
+++ Catchup-Chapters/4-Discovery/Assets/Scripts/HologramPlacement.cs
@@ -0,0 +1,153 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.Windows.Speech;
+using HoloToolkit.Unity;
+using HoloToolkit.Sharing;
+
+public class HologramPlacement : Singleton<HologramPlacement>
+{
+    /// <summary>
+    /// Tracks if we have been sent a tranform for the model.
+    /// The model is rendererd relative to the actual anchor.
+    /// </summary>
+    public bool GotTransform { get; private set; }
+
+    /// <summary>
+    /// When the experience starts, we disable all of the rendering of the model.
+    /// </summary>
+    List<MeshRenderer> disabledRenderers = new List<MeshRenderer>();
+
+    void Start()
+    {
+        // When we first start, we need to disable the model to avoid it obstructing the user picking a hat.
+        DisableModel();
+
+        // We care about getting updates for the model transform.
+        CustomMessages.Instance.MessageHandlers[CustomMessages.TestMessageID.StageTransform] = this.OnStageTransfrom;
+
+        // And when a new user join we will send the model transform we have.
+        SharingSessionTracker.Instance.SessionJoined += Instance_SessionJoined;
+    }
+
+    /// <summary>
+    /// When a new user joins we want to send them the relative transform for the model if we have it.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void Instance_SessionJoined(object sender, SharingSessionTracker.SessionJoinedEventArgs e)
+    {
+        if (GotTransform)
+        {
+            CustomMessages.Instance.SendStageTransform(transform.localPosition, transform.localRotation);
+        }
+    }
+
+    /// <summary>
+    /// Turns off all renderers for the model.
+    /// </summary>
+    void DisableModel()
+    {
+        foreach (MeshRenderer renderer in gameObject.GetComponentsInChildren<MeshRenderer>())
+        {
+            if (renderer.enabled)
+            {
+                renderer.enabled = false;
+                disabledRenderers.Add(renderer);
+            }
+        }
+
+        foreach (MeshCollider collider in gameObject.GetComponentsInChildren<MeshCollider>())
+        {
+            collider.enabled = false;
+        }
+    }
+
+    /// <summary>
+    /// Turns on all renderers that were disabled.
+    /// </summary>
+    void EnableModel()
+    {
+        foreach (MeshRenderer renderer in disabledRenderers)
+        {
+            renderer.enabled = true;
+        }
+
+        foreach (MeshCollider collider in gameObject.GetComponentsInChildren<MeshCollider>())
+        {
+            collider.enabled = true;
+        }
+
+        disabledRenderers.Clear();
+    }
+
+
+    void Update()
+    {
+        // Wait till users pick an avatar to enable renderers.
+        if (disabledRenderers.Count > 0)
+        {
+            if (!PlayerAvatarStore.Instance.PickerActive &&
+            ImportExportAnchorManager.Instance.AnchorEstablished)
+            {
+                // After which we want to start rendering.
+                EnableModel();
+
+                // And if we've already been sent the relative transform, we will use it.
+                if (GotTransform)
+                {
+                    // This triggers the animation sequence for the model and 
+                    // puts the cool materials on the model.
+                    GetComponent<EnergyHubBase>().SendMessage("OnSelect");
+                }
+            }
+        }
+        else if (GotTransform == false)
+        {
+            transform.position = Vector3.Lerp(transform.position, ProposeTransformPosition(), 0.2f);
+        }
+    }
+
+    Vector3 ProposeTransformPosition()
+    {
+        // Put the model 2m in front of the user.
+        Vector3 retval = Camera.main.transform.position + Camera.main.transform.forward * 2;
+
+        return retval;
+    }
+
+    public void OnSelect()
+    {
+        // Note that we have a tranform.
+        GotTransform = true;
+
+        // And send it to our friends.
+        CustomMessages.Instance.SendStageTransform(transform.localPosition, transform.localRotation);
+    }
+
+    /// <summary>
+    /// When a remote system has a transform for us, we'll get it here.
+    /// </summary>
+    /// <param name="msg"></param>
+    void OnStageTransfrom(NetworkInMessage msg)
+    {
+        // We read the user ID but we dont use it here.
+        msg.ReadInt64();
+
+        transform.localPosition = CustomMessages.Instance.ReadVector3(msg);
+        transform.localRotation = CustomMessages.Instance.ReadQuaternion(msg);
+
+        // The first time, we'll want to send the message to the model to do it's animation and
+        // swapt it's materials.
+        if (disabledRenderers.Count == 0 && GotTransform == false)
+        {
+            GetComponent<EnergyHubBase>().SendMessage("OnSelect");
+        }
+
+        GotTransform = true;
+    }
+
+    public void ResetStage()
+    {
+        // We'll use this later.
+    }
+}
