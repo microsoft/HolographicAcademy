01f6baa65095fb8f84cc2a1b564b4e8569361776
Neeraj Wadhwa <neerajw@microsoft.com>
2016-03-30 09:43:46 -0700

    SharedHolograms catchup chapter 1 HoloWorld
    Use this project to recover to a working state.

---
+++ Catchup-Chapters/1-HoloWorld/Assets/Holograms/Support/Orb/GrenadeRotate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrenadeRotate : MonoBehaviour
+{
+
+    public Color EmissiveColor = new Vector4(0f, .87f, 1f, 1f);
+
+    void Start()
+    {
+        var materials = GetComponent<Renderer>().materials;
+        materials[1].SetColor("_EmissionColor", EmissiveColor);
+        GameObject childParticle = transform.Find("ParticleTrail").gameObject;
+        if (childParticle != null)
+        {
+            //Find the child named "ammo" of the gameobject "magazine" (magazine is a child of "gun").
+            childParticle.GetComponent<ParticleSystem>().startColor = EmissiveColor;
+        }
+    }
+}
---
+++ Catchup-Chapters/1-HoloWorld/Assets/Holograms/Support/Sharing/SetStatusColor.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Changes the color on the CursorOffHolograms
+/// to give visual feedback on anchor sharing states.
+/// </summary>
+public class SetStatusColor : MonoBehaviour
+{
+    public Color InitializingColor = Color.magenta;
+    public Color ImportingOrExportingColor = Color.yellow;
+    public Color UploadingColor = Color.blue;
+    public Color DownloadingColor = Color.green;    
+    public Color FailureColor = Color.red;
+
+    ImportExportAnchorManager anchorManager;
+    Color startColor;
+    Light pointLight;
+
+    void Start()
+    {
+        pointLight = GetComponent<Light>();
+        startColor = pointLight.color;
+
+        anchorManager = ImportExportAnchorManager.Instance;
+    }
+
+    void Update()
+    {
+        if (anchorManager != null)
+        {
+            switch (anchorManager.CurrentState)
+            {
+                case ImportExportAnchorManager.ImportExportState.AnchorStore_Initializing:
+                case ImportExportAnchorManager.ImportExportState.Start:
+                case ImportExportAnchorManager.ImportExportState.AnchorStore_Initialized:
+                    pointLight.color = InitializingColor;
+                    break;
+                case ImportExportAnchorManager.ImportExportState.Importing:
+                case ImportExportAnchorManager.ImportExportState.InitialAnchorRequired:
+                case ImportExportAnchorManager.ImportExportState.CreatingInitialAnchor:
+                case ImportExportAnchorManager.ImportExportState.DataReady:
+                    pointLight.color = ImportingOrExportingColor;
+                    break;
+                case ImportExportAnchorManager.ImportExportState.UploadingInitialAnchor:
+                    pointLight.color = UploadingColor;
+                    break;
+                case ImportExportAnchorManager.ImportExportState.DataRequested:
+                    pointLight.color = DownloadingColor;
+                    break;
+                case ImportExportAnchorManager.ImportExportState.Failed:
+                    pointLight.color = FailureColor;
+                    break;
+                default:
+                    pointLight.color = startColor;
+                    break;
+            }
+        }
+
+    }
+}
