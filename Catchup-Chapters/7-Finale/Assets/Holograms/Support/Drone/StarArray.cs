401a0039b182f57f45a45c6d912d347cb027a51d
Neeraj Wadhwa <neerajw@microsoft.com>
2016-03-30 10:14:31 -0700

    SharedHolograms catchup chapter 7 Finale
    Use this to recover to a working state.

---
+++ Catchup-Chapters/7-Finale/Assets/Holograms/Support/Drone/StarArray.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarArray : MonoBehaviour
+{
+
+    public Vector3 RotationVector = new Vector3(1, 0, 0);
+    public int RotationSpeed = 300;
+
+    void Update()
+    {
+        transform.Rotate(RotationVector * Time.deltaTime * RotationSpeed);
+    }
+}
