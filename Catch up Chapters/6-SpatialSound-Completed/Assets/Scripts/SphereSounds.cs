c8737ac9eeb268152a93751a7002d9c5634ac747
Kurtis Eveleigh <kurtie@microsoft.com>
2016-04-04 13:31:45 -0700

    Re-adding Origami...with the spatial mapping updates

---
+++ Catch up Chapters/6-SpatialSound-Completed/Assets/Scripts/SphereSounds.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public class SphereSounds : MonoBehaviour
+{
+    AudioSource audioSource = null;
+    AudioClip impactClip = null;
+    AudioClip rollingClip = null;
+
+    bool rolling = false;
+
+    void Start()
+    {
+        // Add an AudioSource component and set up some defaults
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+        audioSource.spatialize = true;
+        audioSource.spatialBlend = 1.0f;
+        audioSource.dopplerLevel = 0.0f;
+        audioSource.rolloffMode = AudioRolloffMode.Custom;
+
+        // Load the Sphere sounds from the Resources folder
+        impactClip = Resources.Load<AudioClip>("Impact");
+        rollingClip = Resources.Load<AudioClip>("Rolling");
+    }
+
+    // Occurs when this object starts colliding with another object
+    void OnCollisionEnter(Collision collision)
+    {
+        // Play an impact sound if the sphere impacts strongly enough.
+        if (collision.relativeVelocity.magnitude >= 0.1f)
+        {
+            audioSource.clip = impactClip;
+            audioSource.Play();
+        }
+    }
+
+    // Occurs each frame that this object continues to collide with another object
+    void OnCollisionStay(Collision collision)
+    {
+        Rigidbody rigid = this.gameObject.GetComponent<Rigidbody>();
+
+        // Play a rolling sound if the sphere is rolling fast enough.
+        if (!rolling && rigid.velocity.magnitude >= 0.01f)
+        {
+            rolling = true;
+            audioSource.clip = rollingClip;
+            audioSource.Play();
+        }
+        // Stop the rolling sound if rolling slows down.
+        else if (rolling && rigid.velocity.magnitude < 0.01f)
+        {
+            rolling = false;
+            audioSource.Stop();
+        }
+    }
+
+    // Occurs when this object stops colliding with another object
+    void OnCollisionExit(Collision collision)
+    {
+        // Stop the rolling sound if the object falls off and stops colliding.
+        if (rolling)
+        {
+            rolling = false;
+            audioSource.Stop();
+        }
+    }
+}
