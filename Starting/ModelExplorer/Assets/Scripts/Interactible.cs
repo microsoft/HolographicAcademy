80c8687f796467f1ac65b9744867e326c396ed86
Neeraj Wadhwa <neerajw@microsoft.com>
2016-05-11 10:28:47 -0700

    AstroMan and Tagalong performance updates to Gesture class
    Merged changes from Gaze class around AstroMan and TagAlong performance.

--- Starting/ModelExplorer/Assets/Scripts/Interactible.cs
+++ Starting/ModelExplorer/Assets/Scripts/Interactible.cs
@@ -1,45 +1,20 @@
 ﻿using UnityEngine;
 
 /// <summary>
-/// Conveys the set of states available on an Interactible.
-/// In the inspector check the states to display on each Interactible.
-/// </summary>
-[System.Serializable]
-public class InteractibleParameters
-{
-    public bool Scrollable = true;
-    public bool Placeable = true;
-}
-
-/// <summary>
 /// The Interactible class flags a Game Object as being "Interactible".
 /// Determines what happens when an Interactible is being gazed at.
 /// </summary>
 public class Interactible : MonoBehaviour
 {
-    public InteractibleParameters InteractibleParameters;
-
     [Tooltip("Audio clip to play when interacting with this hologram.")]
     public AudioClip TargetFeedbackSound;
     private AudioSource audioSource;
 
     private Material[] defaultMaterials;
-    private Material[] materialsWithHighlight;
-    private Material highlightMaterial;
 
     void Start()
     {
         defaultMaterials = GetComponent<Renderer>().materials;
-
-        highlightMaterial = Resources.Load("Materials/AdditiveRimShader", typeof(Material)) as Material;
-
-        // Add highlightMaterial to materialsWithHighlight.
-        materialsWithHighlight = new Material[defaultMaterials.Length + 1];
-        for (int i = 0; i < defaultMaterials.Length; i++)
-        {
-            materialsWithHighlight[i] = defaultMaterials[i];
-        }
-        materialsWithHighlight[materialsWithHighlight.Length - 1] = highlightMaterial;
 
         // Add a BoxCollider if the interactible does not contain one.
         Collider collider = GetComponentInChildren<Collider>();
@@ -69,30 +44,30 @@ public class Interactible : MonoBehaviour
         }
     }
 
-    void LateUpdate()
-    {
-        Debug.ClearDeveloperConsole();
-    }
-
     void GazeEntered()
     {
-        if (materialsWithHighlight != null && highlightMaterial != null)
+        for (int i = 0; i < defaultMaterials.Length; i++)
         {
-            GetComponent<Renderer>().materials = materialsWithHighlight;
+            defaultMaterials[i].SetFloat("_Highlight", .25f);
         }
     }
 
     void GazeExited()
     {
-        if (defaultMaterials != null && highlightMaterial != null)
+        for (int i = 0; i < defaultMaterials.Length; i++)
         {
-            GetComponent<Renderer>().materials = defaultMaterials;
+            defaultMaterials[i].SetFloat("_Highlight", 0f);
         }
     }
 
     void OnSelect()
     {
-        // Play the audioSource haptic feedback when we gaze and select a hologram.
+        for (int i = 0; i < defaultMaterials.Length; i++)
+        {
+            defaultMaterials[i].SetFloat("_Highlight", .5f);
+        }
+
+        // Play the audioSource feedback when we gaze and select a hologram.
         if (audioSource != null && !audioSource.isPlaying)
         {
             audioSource.Play();
