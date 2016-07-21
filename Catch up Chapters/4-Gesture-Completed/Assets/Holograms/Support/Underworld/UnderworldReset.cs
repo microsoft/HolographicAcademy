1accdf6b9dc1fa63e20e41acd553a5f7c47d5447
Angela Hillier <hillang1@yahoo.com>
2016-07-21 12:27:07 -0700

    Updating Origami course to use new Spatial Mapping components
    Origami will now use the new SpatialMappingRenderer and
    SpatialMappingCollider components from Unity.

--- Catch up Chapters/4-Gesture-Completed/Assets/Holograms/Support/Underworld/UnderworldReset.cs
+++ Catch up Chapters/4-Gesture-Completed/Assets/Holograms/Support/Underworld/UnderworldReset.cs
@@ -18,6 +18,6 @@ public class UnderworldReset : MonoBehaviour
         gameObject.SetActive(false);
 
         // Enable Spatial Mapping again.
-        SpatialMapping.Instance.SetMappingEnabled(true);
+        SpatialMapping.Instance.MappingEnabled = true;
     }
 }
---
+++ Origami/Catch up Chapters/4-Gesture-Completed/Assets/Scripts/SphereCommands.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class SphereCommands : MonoBehaviour
+{
+    // Called by GazeGestureManager when the user performs a Select gesture
+    void OnSelect()
+    {
+        // If the sphere has no Rigidbody component, add one to enable physics.
+        if (!this.GetComponent<Rigidbody>())
+        {
+            var rigidbody = this.gameObject.AddComponent<Rigidbody>();
+            rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        }
+    }
+}
