22bc87b0748d6c75359d7bf9ba0336e314f1507c
Neeraj Wadhwa <neerajw@microsoft.com>
2016-12-01 14:24:27 -0800

    Gesture 211: Renaming HoloToolkit folder name and namespace

---
+++ Completed/ModelExplorer/Assets/HoloToolkit-Gesture-211/Input/Scripts/GazeManager.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+
+namespace Academy.HoloToolkit.Unity
+{
+    /// <summary>
+    /// GazeManager determines the location of the user's gaze, hit position and normals.
+    /// </summary>
+    public class GazeManager : Singleton<GazeManager>
+    {
+        [Tooltip("Maximum gaze distance for calculating a hit.")]
+        public float MaxGazeDistance = 5.0f;
+
+        [Tooltip("Select the layers raycast should target.")]
+        public LayerMask RaycastLayerMask = Physics.DefaultRaycastLayers;
+
+        /// <summary>
+        /// Physics.Raycast result is true if it hits a Hologram.
+        /// </summary>
+        public bool Hit { get; private set; }
+
+        /// <summary>
+        /// HitInfo property gives access
+        /// to RaycastHit public members.
+        /// </summary>
+        public RaycastHit HitInfo { get; private set; }
+
+        /// <summary>
+        /// Position of the user's gaze.
+        /// </summary>
+        public Vector3 Position { get; private set; }
+
+        /// <summary>
+        /// RaycastHit Normal direction.
+        /// </summary>
+        public Vector3 Normal { get; private set; }
+
+        private GazeStabilizer gazeStabilizer;
+        private Vector3 gazeOrigin;
+        private Vector3 gazeDirection;
+
+        void Awake()
+        {
+            gazeStabilizer = GetComponent<GazeStabilizer>();
+        }
+
+        private void Update()
+        {
+            gazeOrigin = Camera.main.transform.position;
+
+            gazeDirection = Camera.main.transform.forward;
+
+            gazeStabilizer.UpdateHeadStability(gazeOrigin, Camera.main.transform.rotation);
+
+            gazeOrigin = gazeStabilizer.StableHeadPosition;
+
+            UpdateRaycast();
+        }
+
+        /// <summary>
+        /// Calculates the Raycast hit position and normal.
+        /// </summary>
+        private void UpdateRaycast()
+        {
+            RaycastHit hitInfo;
+
+            Hit = Physics.Raycast(gazeOrigin,
+                           gazeDirection,
+                           out hitInfo,
+                           MaxGazeDistance,
+                           RaycastLayerMask);
+
+            HitInfo = hitInfo;
+
+            if (Hit)
+            {
+                // If raycast hit a hologram...
+
+                Position = hitInfo.point;
+                Normal = hitInfo.normal;
+            }
+            else
+            {
+                // If raycast did not hit a hologram...
+                // Save defaults ...
+                Position = gazeOrigin + (gazeDirection * MaxGazeDistance);
+                Normal = gazeDirection;
+            }
+        }
+    }
+}
---
+++ Completed/ModelExplorer/Assets/HoloToolkit-Gesture-211/Input/Scripts/HandsManager.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine.VR.WSA.Input;
+using UnityEngine;
+
+namespace Academy.HoloToolkit.Unity
+{
+    /// <summary>
+    /// HandsManager keeps track of when a hand is detected.
+    /// </summary>
+    public class HandsManager : Singleton<HandsManager>
+    {
+        [Tooltip("Audio clip to play when Finger Pressed.")]
+        public AudioClip FingerPressedSound;
+        private AudioSource audioSource;
+
+        /// <summary>
+        /// Tracks the hand detected state.
+        /// </summary>
+        public bool HandDetected
+        {
+            get;
+            private set;
+        }
+
+        // Keeps track of the GameObject that the hand is interacting with.
+        public GameObject FocusedGameObject { get; private set; }
+
+        void Awake()
+        {
+            EnableAudioHapticFeedback();
+
+            InteractionManager.SourceDetected += InteractionManager_SourceDetected;
+            InteractionManager.SourceLost += InteractionManager_SourceLost;
+
+            /* TODO: DEVELOPER CODE ALONG 2.a */
+
+            // 2.a: Register for SourceManager.SourcePressed event.
+            InteractionManager.SourcePressed += InteractionManager_SourcePressed;
+
+            // 2.a: Register for SourceManager.SourceReleased event.
+            InteractionManager.SourceReleased += InteractionManager_SourceReleased;
+
+            // 2.a: Initialize FocusedGameObject as null.
+            FocusedGameObject = null;
+        }
+
+        private void EnableAudioHapticFeedback()
+        {
+            // If this hologram has an audio clip, add an AudioSource with this clip.
+            if (FingerPressedSound != null)
+            {
+                audioSource = GetComponent<AudioSource>();
+                if (audioSource == null)
+                {
+                    audioSource = gameObject.AddComponent<AudioSource>();
+                }
+
+                audioSource.clip = FingerPressedSound;
+                audioSource.playOnAwake = false;
+                audioSource.spatialBlend = 1;
+                audioSource.dopplerLevel = 0;
+            }
+        }
+
+        private void InteractionManager_SourceDetected(InteractionSourceState hand)
+        {
+            HandDetected = true;
+        }
+
+        private void InteractionManager_SourceLost(InteractionSourceState hand)
+        {
+            HandDetected = false;
+
+            // 2.a: Reset FocusedGameObject.
+            ResetFocusedGameObject();
+        }
+
+        private void InteractionManager_SourcePressed(InteractionSourceState hand)
+        {
+            if (InteractibleManager.Instance.FocusedGameObject != null)
+            {
+                // Play a select sound if we have an audio source and are not targeting an asset with a select sound.
+                if (audioSource != null && !audioSource.isPlaying &&
+                    (InteractibleManager.Instance.FocusedGameObject.GetComponent<Interactible>() != null &&
+                    InteractibleManager.Instance.FocusedGameObject.GetComponent<Interactible>().TargetFeedbackSound == null))
+                {
+                    audioSource.Play();
+                }
+
+                // 2.a: Cache InteractibleManager's FocusedGameObject in FocusedGameObject.
+                FocusedGameObject = InteractibleManager.Instance.FocusedGameObject;
+            }
+        }
+
+        private void InteractionManager_SourceReleased(InteractionSourceState hand)
+        {
+            // 2.a: Reset FocusedGameObject.
+            ResetFocusedGameObject();
+        }
+
+        private void ResetFocusedGameObject()
+        {
+            // 2.a: Set FocusedGameObject to be null.
+            FocusedGameObject = null;
+
+            // 2.a: On GestureManager call ResetGestureRecognizers
+            // to complete any currently active gestures.
+            GestureManager.Instance.ResetGestureRecognizers();
+        }
+
+        void OnDestroy()
+        {
+            InteractionManager.SourceDetected -= InteractionManager_SourceDetected;
+            InteractionManager.SourceLost -= InteractionManager_SourceLost;
+
+            // 2.a: Unregister the SourceManager.SourceReleased event.
+            InteractionManager.SourceReleased -= InteractionManager_SourceReleased;
+
+            // 2.a: Unregister for SourceManager.SourcePressed event.
+            InteractionManager.SourcePressed -= InteractionManager_SourcePressed;
+        }
+    }
+}
