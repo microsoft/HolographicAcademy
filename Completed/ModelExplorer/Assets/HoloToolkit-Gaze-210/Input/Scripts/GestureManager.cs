293bf323f3457354c89d3c2bc9e6b736ae586411
Neeraj Wadhwa <neerajw@microsoft.com>
2016-12-01 12:31:30 -0800

    Gaze 210: Renaming HoloToolkit folder name and namespace
    This helps address issue #16 and makes it easier for developers to go
    from Academy's teachable HoloToolkit to the public GitHub latest
    version.
    
    Also upgraded to Unity 5.5f3.

---
+++ Completed/ModelExplorer/Assets/HoloToolkit-Gaze-210/Input/Scripts/GestureManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.VR.WSA.Input;
+
+namespace Academy.HoloToolkit.Unity
+{
+    /// <summary>
+    /// GestureManager contains event handlers for subscribed gestures.
+    /// </summary>
+    public class GestureManager : MonoBehaviour
+    {
+        private GestureRecognizer gestureRecognizer;
+
+        void Start()
+        {
+            gestureRecognizer = new GestureRecognizer();
+            gestureRecognizer.SetRecognizableGestures(GestureSettings.Tap);
+
+            gestureRecognizer.TappedEvent += (source, tapCount, ray) =>
+            {
+                GameObject focusedObject = InteractibleManager.Instance.FocusedGameObject;
+
+                if (focusedObject != null)
+                {
+                    focusedObject.SendMessage("OnSelect");
+                }
+            };
+
+            gestureRecognizer.StartCapturingGestures();
+        }
+
+        void OnDestroy()
+        {
+            gestureRecognizer.StopCapturingGestures();
+        }
+    }
+}
