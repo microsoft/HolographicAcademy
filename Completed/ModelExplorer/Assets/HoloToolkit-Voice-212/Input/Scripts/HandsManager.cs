1f1ef82f24d1d2d5f253f63337e1480fa2a73486
Neeraj Wadhwa <neerajw@microsoft.com>
2016-12-02 13:31:21 -0800

    Voice 212: Renaming HoloToolkit folder name and namespace
    This helps address issue #16 and making it easier for developers to go
    from Academy's teachable HoloToolkit to the public GitHub latest
    version.
    
    Also upgraded to Unity 5.50f3.

---
+++ Completed/ModelExplorer/Assets/HoloToolkit-Voice-212/Input/Scripts/HandsManager.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine.VR.WSA.Input;
+using UnityEngine;
+
+namespace Academy.HoloToolkit.Unity
+{
+    /// <summary>
+    /// HandsManager keeps track of when a hand is detected.
+    /// </summary>
+    public class HandsManager : Singleton<HandsManager>
+    {
+        [Tooltip("Audio clip to play when Finger Pressed.")]
+        public AudioClip FingerPressedSound;
+        private AudioSource audioSource;
+
+        /// <summary>
+        /// Tracks the hand detected state.
+        /// </summary>
+        public bool HandDetected
+        {
+            get;
+            private set;
+        }
+
+        // Keeps track of the GameObject that the hand is interacting with.
+        public GameObject FocusedGameObject { get; private set; }
+
+        void Awake()
+        {
+            EnableAudioHapticFeedback();
+
+            InteractionManager.SourceDetected += InteractionManager_SourceDetected;
+            InteractionManager.SourceLost += InteractionManager_SourceLost;
+
+            InteractionManager.SourcePressed += InteractionManager_SourcePressed;
+            InteractionManager.SourceReleased += InteractionManager_SourceReleased;
+
+            FocusedGameObject = null;
+        }
+
+        private void EnableAudioHapticFeedback()
+        {
+            // If this hologram has an audio clip, add an AudioSource with this clip.
+            if (FingerPressedSound != null)
+            {
+                audioSource = GetComponent<AudioSource>();
+                if (audioSource == null)
+                {
+                    audioSource = gameObject.AddComponent<AudioSource>();
+                }
+
+                audioSource.clip = FingerPressedSound;
+                audioSource.playOnAwake = false;
+                audioSource.spatialBlend = 1;
+                audioSource.dopplerLevel = 0;
+            }
+        }
+
+        private void InteractionManager_SourceDetected(InteractionSourceState hand)
+        {
+            HandDetected = true;
+        }
+
+        private void InteractionManager_SourceLost(InteractionSourceState hand)
+        {
+            HandDetected = false;
+
+            ResetFocusedGameObject();
+        }
+
+        private void InteractionManager_SourcePressed(InteractionSourceState hand)
+        {
+            if (InteractibleManager.Instance.FocusedGameObject != null)
+            {
+                // Play a select sound if we have an audio source and are not targeting an asset with a select sound.
+                if (audioSource != null && !audioSource.isPlaying &&
+                    (InteractibleManager.Instance.FocusedGameObject.GetComponent<Interactible>() != null &&
+                    InteractibleManager.Instance.FocusedGameObject.GetComponent<Interactible>().TargetFeedbackSound == null))
+                {
+                    audioSource.Play();
+                }
+
+                FocusedGameObject = InteractibleManager.Instance.FocusedGameObject;
+            }
+        }
+
+        private void InteractionManager_SourceReleased(InteractionSourceState hand)
+        {
+            ResetFocusedGameObject();
+        }
+
+        private void ResetFocusedGameObject()
+        {
+            FocusedGameObject = null;
+
+            GestureManager.Instance.ResetGestureRecognizers();
+        }
+
+        void OnDestroy()
+        {
+            InteractionManager.SourceDetected -= InteractionManager_SourceDetected;
+            InteractionManager.SourceLost -= InteractionManager_SourceLost;
+
+            InteractionManager.SourceReleased -= InteractionManager_SourceReleased;
+            InteractionManager.SourcePressed -= InteractionManager_SourcePressed;
+        }
+    }
+}
---
+++ Starting/ModelExplorer/Assets/HoloToolkit-Voice-212/Input/Scripts/GestureManager.cs
@@ -0,0 +1,175 @@
+﻿using UnityEngine;
+using UnityEngine.VR.WSA.Input;
+
+namespace Academy.HoloToolkit.Unity
+{
+    public class GestureManager : Singleton<GestureManager>
+    {
+        // Tap and Navigation gesture recognizer.
+        public GestureRecognizer NavigationRecognizer { get; private set; }
+
+        // Manipulation gesture recognizer.
+        public GestureRecognizer ManipulationRecognizer { get; private set; }
+
+        // Currently active gesture recognizer.
+        public GestureRecognizer ActiveRecognizer { get; private set; }
+
+        public bool IsNavigating { get; private set; }
+
+        public Vector3 NavigationPosition { get; private set; }
+
+        public bool IsManipulating { get; private set; }
+
+        public Vector3 ManipulationPosition { get; private set; }
+
+        void Awake()
+        {
+            NavigationRecognizer = new GestureRecognizer();
+
+            NavigationRecognizer.SetRecognizableGestures(
+                GestureSettings.Tap |
+                GestureSettings.NavigationX);
+
+            NavigationRecognizer.TappedEvent += NavigationRecognizer_TappedEvent;
+            NavigationRecognizer.NavigationStartedEvent += NavigationRecognizer_NavigationStartedEvent;
+            NavigationRecognizer.NavigationUpdatedEvent += NavigationRecognizer_NavigationUpdatedEvent;
+            NavigationRecognizer.NavigationCompletedEvent += NavigationRecognizer_NavigationCompletedEvent;
+            NavigationRecognizer.NavigationCanceledEvent += NavigationRecognizer_NavigationCanceledEvent;
+
+            // Instantiate the ManipulationRecognizer.
+            ManipulationRecognizer = new GestureRecognizer();
+
+            // Add the ManipulationTranslate GestureSetting to the ManipulationRecognizer's RecognizableGestures.
+            ManipulationRecognizer.SetRecognizableGestures(
+                GestureSettings.ManipulationTranslate);
+
+            // Register for the Manipulation events on the ManipulationRecognizer.
+            ManipulationRecognizer.ManipulationStartedEvent += ManipulationRecognizer_ManipulationStartedEvent;
+            ManipulationRecognizer.ManipulationUpdatedEvent += ManipulationRecognizer_ManipulationUpdatedEvent;
+            ManipulationRecognizer.ManipulationCompletedEvent += ManipulationRecognizer_ManipulationCompletedEvent;
+            ManipulationRecognizer.ManipulationCanceledEvent += ManipulationRecognizer_ManipulationCanceledEvent;
+
+            ResetGestureRecognizers();
+        }
+
+        void OnDestroy()
+        {
+            // Unregister the Tapped and Navigation events on the NavigationRecognizer.
+            NavigationRecognizer.TappedEvent -= NavigationRecognizer_TappedEvent;
+
+            NavigationRecognizer.NavigationStartedEvent -= NavigationRecognizer_NavigationStartedEvent;
+            NavigationRecognizer.NavigationUpdatedEvent -= NavigationRecognizer_NavigationUpdatedEvent;
+            NavigationRecognizer.NavigationCompletedEvent -= NavigationRecognizer_NavigationCompletedEvent;
+            NavigationRecognizer.NavigationCanceledEvent -= NavigationRecognizer_NavigationCanceledEvent;
+
+            // Unregister the Manipulation events on the ManipulationRecognizer.
+            ManipulationRecognizer.ManipulationStartedEvent -= ManipulationRecognizer_ManipulationStartedEvent;
+            ManipulationRecognizer.ManipulationUpdatedEvent -= ManipulationRecognizer_ManipulationUpdatedEvent;
+            ManipulationRecognizer.ManipulationCompletedEvent -= ManipulationRecognizer_ManipulationCompletedEvent;
+            ManipulationRecognizer.ManipulationCanceledEvent -= ManipulationRecognizer_ManipulationCanceledEvent;
+        }
+
+        /// <summary>
+        /// Revert back to the default GestureRecognizer.
+        /// </summary>
+        public void ResetGestureRecognizers()
+        {
+            // Default to the navigation gestures.
+            Transition(NavigationRecognizer);
+        }
+
+        /// <summary>
+        /// Transition to a new GestureRecognizer.
+        /// </summary>
+        /// <param name="newRecognizer">The GestureRecognizer to transition to.</param>
+        public void Transition(GestureRecognizer newRecognizer)
+        {
+            if (newRecognizer == null)
+            {
+                return;
+            }
+
+            if (ActiveRecognizer != null)
+            {
+                if (ActiveRecognizer == newRecognizer)
+                {
+                    return;
+                }
+
+                ActiveRecognizer.CancelGestures();
+                ActiveRecognizer.StopCapturingGestures();
+            }
+
+            newRecognizer.StartCapturingGestures();
+            ActiveRecognizer = newRecognizer;
+        }
+
+        private void NavigationRecognizer_NavigationStartedEvent(InteractionSourceKind source, Vector3 relativePosition, Ray ray)
+        {
+            IsNavigating = true;
+
+            NavigationPosition = relativePosition;
+        }
+
+        private void NavigationRecognizer_NavigationUpdatedEvent(InteractionSourceKind source, Vector3 relativePosition, Ray ray)
+        {
+            IsNavigating = true;
+
+            NavigationPosition = relativePosition;
+        }
+
+        private void NavigationRecognizer_NavigationCompletedEvent(InteractionSourceKind source, Vector3 relativePosition, Ray ray)
+        {
+            IsNavigating = false;
+        }
+
+        private void NavigationRecognizer_NavigationCanceledEvent(InteractionSourceKind source, Vector3 relativePosition, Ray ray)
+        {
+            IsNavigating = false;
+        }
+
+        private void ManipulationRecognizer_ManipulationStartedEvent(InteractionSourceKind source, Vector3 position, Ray ray)
+        {
+            if (HandsManager.Instance.FocusedGameObject != null)
+            {
+                IsManipulating = true;
+
+                ManipulationPosition = position;
+
+                HandsManager.Instance.FocusedGameObject.SendMessageUpwards("PerformManipulationStart", position);
+            }
+        }
+
+        private void ManipulationRecognizer_ManipulationUpdatedEvent(InteractionSourceKind source, Vector3 position, Ray ray)
+        {
+            if (HandsManager.Instance.FocusedGameObject != null)
+            {
+                IsManipulating = true;
+
+                ManipulationPosition = position;
+
+                HandsManager.Instance.FocusedGameObject.SendMessageUpwards("PerformManipulationUpdate", position);
+            }
+        }
+
+        private void ManipulationRecognizer_ManipulationCompletedEvent(InteractionSourceKind source, Vector3 position, Ray ray)
+        {
+            IsManipulating = false;
+        }
+
+        private void ManipulationRecognizer_ManipulationCanceledEvent(InteractionSourceKind source, Vector3 position, Ray ray)
+        {
+            IsManipulating = false;
+        }
+
+        private void NavigationRecognizer_TappedEvent(InteractionSourceKind source, int tapCount, Ray ray)
+        {
+            GameObject focusedObject = InteractibleManager.Instance.FocusedGameObject;
+
+            if (focusedObject != null)
+            {
+                focusedObject.SendMessageUpwards("OnSelect");
+            }
+        }
+    }
+}
