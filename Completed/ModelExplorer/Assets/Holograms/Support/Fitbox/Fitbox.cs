36fe149e1ee77175a3a21a4bdf860222d7f00dee
Kurtis Eveleigh <kurtie@microsoft.com>
2018-04-24 16:59:09 -0700

    Adding license headers

--- Completed/ModelExplorer/Assets/Holograms/Support/Fitbox/Fitbox.cs
+++ Completed/ModelExplorer/Assets/Holograms/Support/Fitbox/Fitbox.cs
@@ -1,4 +1,7 @@
-﻿using HoloToolkit.Unity;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using HoloToolkit.Unity;
 using UnityEngine;
 using UnityEngine.XR;
 using UnityEngine.XR.WSA.Input;
--- Completed/ModelExplorer/Assets/Scripts/TagalongAction.cs
+++ Completed/ModelExplorer/Assets/Scripts/TagalongAction.cs
@@ -1,4 +1,7 @@
-﻿using HoloToolkit.Unity;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using HoloToolkit.Unity;
 using UnityEngine;
 
 public class TagalongAction : InteractibleAction
