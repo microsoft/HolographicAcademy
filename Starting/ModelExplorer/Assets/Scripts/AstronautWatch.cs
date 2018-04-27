df63164a34d714a266673f36d5ea81d96229e47d
Kurtis Eveleigh <kurtie@microsoft.com>
2018-04-27 15:18:52 -0700

    Updating Starting with Completed state

--- Starting/ModelExplorer/Assets/Scripts/AstronautWatch.cs
+++ Starting/ModelExplorer/Assets/Scripts/AstronautWatch.cs
@@ -1,93 +1,94 @@
-﻿using Academy.HoloToolkit.Unity;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using HoloToolkit.Unity;
+using HoloToolkit.Unity.InputModule;
 using UnityEngine;
 
-[RequireComponent(typeof(KeywordManager))]
-public class AstronautWatch : Singleton<AstronautWatch>
+namespace Academy
 {
-    [Tooltip("Drag the Communicator prefab asset.")]
-    public GameObject CommunicatorPrefab;
-    private GameObject communicatorGameObject;
-
-    [Tooltip("Drag the Message Received prefab asset.")]
-    public GameObject MessagePrefab;
-    private GameObject messageGameObject;
-
-    [Tooltip("Drag the Voice Tooltip prefab asset.")]
-    public GameObject OpenCommunicatorTooltip;
-    private GameObject openCommunicatorTooltipGameObject;
-
-    public AudioClip DismissSound;
-
-    public bool CommunicatorOpen { get; private set; }
-
-    private KeywordManager keywordManager;
-
-    void Awake()
+    [RequireComponent(typeof(SpeechInputSource))]
+    public class AstronautWatch : Singleton<AstronautWatch>, IFocusable
     {
-        CommunicatorOpen = false;
+        [Tooltip("Drag the Communicator prefab asset.")]
+        public GameObject CommunicatorPrefab;
+        private GameObject communicatorGameObject;
 
-        openCommunicatorTooltipGameObject = Instantiate(OpenCommunicatorTooltip);
+        [Tooltip("Drag the Message Received prefab asset.")]
+        public GameObject MessagePrefab;
+        private GameObject messageGameObject;
 
-        openCommunicatorTooltipGameObject.transform.position = new Vector3(
-            gameObject.transform.position.x + 0.1f,
-            gameObject.transform.position.y + 0.05f,
-            gameObject.transform.position.z - 0.05f);
+        [Tooltip("Drag the Voice Tooltip prefab asset.")]
+        public GameObject OpenCommunicatorTooltip;
+        private GameObject openCommunicatorTooltipGameObject;
 
-        openCommunicatorTooltipGameObject.transform.parent = gameObject.transform;
-        openCommunicatorTooltipGameObject.SetActive(false);
+        public AudioClip DismissSound;
 
-        keywordManager = GetComponent<KeywordManager>();
-    }
+        public bool CommunicatorOpen { get; private set; }
 
-    public void OpenCommunicator()
-    {
-        // When a voice command is heard, change the text color on the tooltip.
-        // This gives feedback to the user that the voice command has been heard.
-        openCommunicatorTooltipGameObject.GetComponent<VoiceTooltip>().VoiceCommandHeard();
-
-        CommunicatorOpen = true;
-
-        communicatorGameObject = Instantiate(CommunicatorPrefab);
-
-        communicatorGameObject.transform.position = transform.position;
-        communicatorGameObject.transform.Translate(0.4f, 0.0f, 0.0f, Camera.main.transform);
-    }
-
-    public void CloseCommunicator()
-    {
-        CommunicatorOpen = false;
-
-        GameObject soundPlayer = new GameObject("MessageSentSound");
-        AudioSource soundSource = soundPlayer.AddComponent<AudioSource>();
-        soundSource.clip = DismissSound;
-        soundSource.Play();
-
-        messageGameObject = (GameObject)Instantiate(MessagePrefab, communicatorGameObject.transform.position, MessagePrefab.transform.rotation);
-
-        Destroy(communicatorGameObject);
-        Destroy(messageGameObject, 1.0f);
-        Destroy(soundPlayer, DismissSound.length);
-    }
-
-    void GazeEntered()
-    {
-        // If communicator is not open, show the voice command tooltip.
-        if(!CommunicatorOpen)
+        protected override void Awake()
         {
-            openCommunicatorTooltipGameObject.SetActive(true);
+            base.Awake();
 
-            keywordManager.StartKeywordRecognizer();
+            CommunicatorOpen = false;
+
+            openCommunicatorTooltipGameObject = Instantiate(OpenCommunicatorTooltip);
+
+            openCommunicatorTooltipGameObject.transform.position = new Vector3(
+                gameObject.transform.position.x + 0.1f,
+                gameObject.transform.position.y + 0.05f,
+                gameObject.transform.position.z - 0.05f);
+
+            openCommunicatorTooltipGameObject.transform.parent = gameObject.transform;
+            openCommunicatorTooltipGameObject.SetActive(false);
         }
-    }
 
-    void GazeExited()
-    {
-        // Hide tooltip when user looks away.
-        openCommunicatorTooltipGameObject.SetActive(false);
+        public void OpenCommunicator()
+        {
+            // When a voice command is heard, change the text color on the tooltip.
+            // This gives feedback to the user that the voice command has been heard.
+            openCommunicatorTooltipGameObject.GetComponent<VoiceTooltip>().VoiceCommandHeard();
 
-        keywordManager.StopKeywordRecognizer();
+            CommunicatorOpen = true;
 
-        // Reset tooltip to its original state.
-        openCommunicatorTooltipGameObject.GetComponent<VoiceTooltip>().ResetTooltip();
+            communicatorGameObject = Instantiate(CommunicatorPrefab);
+
+            communicatorGameObject.transform.position = transform.position;
+            communicatorGameObject.transform.Translate(0.4f, 0.0f, 0.0f, Camera.main.transform);
+        }
+
+        public void CloseCommunicator()
+        {
+            CommunicatorOpen = false;
+
+            GameObject soundPlayer = new GameObject("MessageSentSound");
+            AudioSource soundSource = soundPlayer.AddComponent<AudioSource>();
+            soundSource.clip = DismissSound;
+            soundSource.Play();
+
+            messageGameObject = Instantiate(MessagePrefab, communicatorGameObject.transform.position, MessagePrefab.transform.rotation);
+
+            Destroy(communicatorGameObject);
+            Destroy(messageGameObject, 1.0f);
+            Destroy(soundPlayer, DismissSound.length);
+        }
+
+        void IFocusable.OnFocusEnter()
+        {
+            // If communicator is not open, show the voice command tooltip.
+            if (!CommunicatorOpen)
+            {
+                openCommunicatorTooltipGameObject.SetActive(true);
+            }
+        }
+
+        void IFocusable.OnFocusExit()
+        {
+            // Hide tooltip when user looks away.
+            openCommunicatorTooltipGameObject.SetActive(false);
+
+            // Reset tooltip to its original state.
+            openCommunicatorTooltipGameObject.GetComponent<VoiceTooltip>().ResetTooltip();
+        }
     }
 }
--- Starting/ModelExplorer/Assets/Scripts/GestureAction.cs
+++ Starting/ModelExplorer/Assets/Scripts/GestureAction.cs
@@ -1,51 +1,107 @@
-﻿using Academy.HoloToolkit.Unity;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using HoloToolkit.Unity.InputModule;
 using UnityEngine;
 
-/// <summary>
-/// GestureAction performs custom actions based on 
-/// which gesture is being performed.
-/// </summary>
-public class GestureAction : MonoBehaviour
+namespace Academy
 {
-    [Tooltip("Rotation max speed controls amount of rotation.")]
-    public float RotationSensitivity = 10.0f;
-
-    private Vector3 manipulationPreviousPosition;
-
-    private float rotationFactor;
-
-    void Update()
+    /// <summary>
+    /// GestureAction performs custom actions based on 
+    /// which gesture is being performed.
+    /// </summary>
+    public class GestureAction : MonoBehaviour, INavigationHandler, IManipulationHandler, ISpeechHandler
     {
-        PerformRotation();
-    }
+        [Tooltip("Rotation max speed controls amount of rotation.")]
+        [SerializeField]
+        private float RotationSensitivity = 10.0f;
 
-    private void PerformRotation()
-    {
-        if (GestureManager.Instance.IsNavigating &&
-            (!ExpandModel.Instance.IsModelExpanded ||
-            (ExpandModel.Instance.IsModelExpanded && HandsManager.Instance.FocusedGameObject == gameObject)))
+        private bool isNavigationEnabled = true;
+        public bool IsNavigationEnabled
         {
-            // This will help control the amount of rotation.
-            rotationFactor = GestureManager.Instance.NavigationPosition.x * RotationSensitivity;
-
-            transform.Rotate(new Vector3(0, -1 * rotationFactor, 0));
+            get { return isNavigationEnabled; }
+            set { isNavigationEnabled = value; }
         }
-    }
 
-    void PerformManipulationStart(Vector3 position)
-    {
-        manipulationPreviousPosition = position;
-    }
+        private Vector3 manipulationOriginalPosition = Vector3.zero;
 
-    void PerformManipulationUpdate(Vector3 position)
-    {
-        if (GestureManager.Instance.IsManipulating)
+        void INavigationHandler.OnNavigationStarted(NavigationEventData eventData)
         {
-            Vector3 moveVector = Vector3.zero;
-            moveVector = position - manipulationPreviousPosition;
-            manipulationPreviousPosition = position;
+            InputManager.Instance.PushModalInputHandler(gameObject);
+        }
 
-            transform.position += moveVector;
+        void INavigationHandler.OnNavigationUpdated(NavigationEventData eventData)
+        {
+            if (isNavigationEnabled)
+            {
+                /* TODO: DEVELOPER CODING EXERCISE 2.c */
+
+                // 2.c: Calculate a float rotationFactor based on eventData's NormalizedOffset.x multiplied by RotationSensitivity.
+                // This will help control the amount of rotation.
+                float rotationFactor = eventData.NormalizedOffset.x * RotationSensitivity;
+
+                // 2.c: transform.Rotate around the Y axis using rotationFactor.
+                transform.Rotate(new Vector3(0, -1 * rotationFactor, 0));
+            }
+        }
+
+        void INavigationHandler.OnNavigationCompleted(NavigationEventData eventData)
+        {
+            InputManager.Instance.PopModalInputHandler();
+        }
+
+        void INavigationHandler.OnNavigationCanceled(NavigationEventData eventData)
+        {
+            InputManager.Instance.PopModalInputHandler();
+        }
+
+        void IManipulationHandler.OnManipulationStarted(ManipulationEventData eventData)
+        {
+            if (!isNavigationEnabled)
+            {
+                InputManager.Instance.PushModalInputHandler(gameObject);
+
+                manipulationOriginalPosition = transform.position;
+            }
+        }
+
+        void IManipulationHandler.OnManipulationUpdated(ManipulationEventData eventData)
+        {
+            if (!isNavigationEnabled)
+            {
+                /* TODO: DEVELOPER CODING EXERCISE 4.a */
+
+                // 4.a: Make this transform's position be the manipulationOriginalPosition + eventData.CumulativeDelta
+                transform.position = manipulationOriginalPosition + eventData.CumulativeDelta;
+            }
+        }
+
+        void IManipulationHandler.OnManipulationCompleted(ManipulationEventData eventData)
+        {
+            InputManager.Instance.PopModalInputHandler();
+        }
+
+        void IManipulationHandler.OnManipulationCanceled(ManipulationEventData eventData)
+        {
+            InputManager.Instance.PopModalInputHandler();
+        }
+
+        void ISpeechHandler.OnSpeechKeywordRecognized(SpeechEventData eventData)
+        {
+            if (eventData.RecognizedText.ToLower().Equals("move astronaut"))
+            {
+                isNavigationEnabled = false;
+            }
+            else if (eventData.RecognizedText.ToLower().Equals("rotate astronaut"))
+            {
+                isNavigationEnabled = true;
+            }
+            else
+            {
+                return;
+            }
+
+            eventData.Use();
         }
     }
 }
