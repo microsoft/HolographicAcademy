5081f13b2ede9a3307367d377cc4389de190fcc1
Kurtis Eveleigh <kurtie@microsoft.com>
2018-06-22 14:51:19 -0700

    Reconcile Academy TapToPlace with Toolkit TapToPlace

---
+++ Starting/Decibel/Assets/Scripts/TapToPlaceWithSoundAndAction.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Academy
+{
+    public class TapToPlaceWithSoundAndAction : TapToPlaceWithSound
+    {
+        [SerializeField]
+        private UnityEvent onPickupAction;
+
+        [SerializeField]
+        private UnityEvent onPlacementAction;
+
+        public override void OnInputClicked(InputClickedEventData eventData)
+        {
+            base.OnInputClicked(eventData);
+
+            if (IsBeingPlaced)
+            {
+                onPickupAction.Invoke();
+            }
+            else
+            {
+                onPlacementAction.Invoke();
+            }
+        }
+
+    }
+}
